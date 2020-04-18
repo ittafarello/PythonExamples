446269f1a05a6c9de2b27c8594da554711a1c73f
itamart <itamartafarello@gmail.com>
2020-04-18 17:19:25 -0300

    added another example

--- Strings/formatting.py
+++ Strings/formatting.py
@@ -8,3 +8,6 @@ myformatting = "This is a formatting {} {} {}"
 print(myformatting.format(varnumber,
                           vartext,
                           varboolean))
+
+phrase = "My name is {1} and I am {0} years old and living in {2}"
+print(phrase.format(53, "Itamar Tafarello", "Brazil"))
