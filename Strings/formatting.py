57676a825501353ae82345c5a247d395e4fda91e
itamart <itamartafarello@gmail.com>
2020-04-18 17:45:03 -0300

    add center,left,right

--- Strings/formatting.py
+++ Strings/formatting.py
@@ -11,3 +11,27 @@ print(myformatting.format(varnumber,
 
 phrase = "My name is {1} and I am {0} years old and living in {2}"
 print(phrase.format(53, "Itamar Tafarello", "Brazil"))
+
+format_string = "{artist} sang {song} in {year}"
+print(format_string.format(artist="Caetano", year=2019, song="Leaozinho"))
+
+'''
+format an string with 25 characters {:25}
+'''
+format_string = "| {:25}|"
+print(format_string.format("Itamar"))
+
+'''
+format an string with 25 characters and aligning the string
+>right
+<left
+^center
+'''
+format_left = "| {:<25}|"
+format_right = "| {:>25}|"
+format_center = "| {:^25}|"
+print(format_left.format("Itamar"))
+print(format_center.format("Itamar"))
+print(format_right.format("Itamar"))
+
+
