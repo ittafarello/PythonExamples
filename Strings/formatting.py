1299b2d0260c7bd7ce2191e347514f11f7814fda
itamart <itamartafarello@gmail.com>
2020-04-18 17:13:08 -0300

    This is a first python program

---
+++ Strings/formatting.py
@@ -0,0 +1,10 @@
+'''
+This examples will show how to use the formmatting function
+'''
+varnumber = 10
+vartext = "Itamar"
+varboolean = True
+myformatting = "This is a formatting {} {} {}"
+print(myformatting.format(varnumber,
+                          vartext,
+                          varboolean))
