9e4c452d062a063ab13fc666b4a7cfb573ec1056
itamart <itamartafarello@gmail.com>
2020-04-19 14:52:30 -0300

    added new code

---
+++ Data Science/neuronnetwork.py
@@ -0,0 +1,70 @@
+import numpy as np
+import logging
+
+logger = logging.getLogger("myLog")
+
+def setlog():
+    logger.setLevel(logging.DEBUG)
+    logger_file = logging.FileHandler("app.log")
+    logger_file.setFormatter(logging.Formatter("%(levelname)s:%(asctime)s:%(message)s"))
+    logger.addHandler(logger_file)
+
+
+def sigmoid(x):
+    return 1.0 / (1 + np.exp(-x))
+
+
+def sigmoid_derivative(x):
+    return x * (1.0 - x)
+
+
+class NeuralNetwork:
+    def __init__(self, x, y):
+        self.input = x
+        self.weights1 = np.random.rand(self.input.shape[1], 4)
+        self.weights2 = np.random.rand(4, 1)
+        self.y = y
+        self.output = np.zeros(self.y.shape)
+
+    def viewinitialsettings(self):
+        logger.info(f"\nInput = {self.input}\n \nWeights 1 = {self.weights1}\n \nWeights 2 = {self.weights2}\n \ny = {self.y}\n \nOutput = {self.output}")
+
+
+#    def viewmatriz(self):
+#        for index, value in enumarate(inputs):
+
+
+    def feedfoward(self):
+        self.layer1 = sigmoid(np.dot(self.input, self.weights1))
+        self.output = sigmoid(np.dot(self.layer1, self.weights2))
+
+    def backprop(self):
+        d_weights2 = np.dot(self.layer1.T,
+                            (2 * (self.y - self.output) * sigmoid_derivative(self.output)))
+
+        d_weights1 = np.dot(self.input.T, (np.dot(2 * (self.y - self.output) *
+                                                  sigmoid_derivative(self.output), self.weights2.T) *
+                                           sigmoid_derivative(self.layer1)))
+
+        self.weights1 += d_weights1
+        self.weights2 += d_weights2
+
+
+if __name__ == "__main__":
+    setlog()
+
+    X = np.array([[0, 0, 1],
+                  [0, 1, 1],
+                  [1, 0, 1],
+                  [1, 1, 1]])
+    y = np.array([[0], [1], [1], [0]])
+
+    nn = NeuralNetwork(X,y)
+
+    nn.viewinitialsettings()
+
+    for i in range(1500):
+        nn.feedfoward()
+        nn.backprop()
+
+    print(nn.output)
